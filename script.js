adc7fe4207d41aafb33706109109591bc09c96af
Prashant Iratkar <prashantiratkar@gmail.com>
2024-03-09 15:51:35 +0530

    frist commit

---
+++ script.js
@@ -0,0 +1,19 @@
+//---declare the variable to store the value---//
+let imgBox = document.getElementById("imgBox")
+let qrImage = document.getElementById("qrImage")
+let qrText = document.getElementById("qrText")
+
+//-----for generate the QR code from URL--//
+function generateQR(){
+    if(qrText.value.length > 0){
+    qrImage.src ="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+
+    imgBox.classList.add("show-img")
+}
+else{
+    qrText.classList.add("error");
+    setTimeout(()=>{
+        qrText.classList.remove("error");
+    },1000);
+}
+}
